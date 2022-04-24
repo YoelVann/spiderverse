a8b70a6fae1b9c770fea6852ef56a18d446b0ac5
Joel Hoil <yoelvann@gmail.com>
2022-04-24 00:33:46 -0500

    add test passed

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,8 +1,10 @@
+const Spiderman = require('./../app/spiderman');
+
 describe("Unit Tests for Spiderman class", () => {
     test('1) Create a spiderman object', () => {
-      const andrewGarfield = new Spiderman("Spider Sony", 31, "Andrew Garfield", 2, "Sony")
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
      
-      expect(andrewGarfield.name).toBe("Spiderma Sony");
+      expect(andrewGarfield.name).toBe("Spiderman Sony");
       expect(andrewGarfield.age).toBe(31);
       expect(andrewGarfield.actor).toBe("Andrew Garfield");
       expect(andrewGarfield.movies).toBe(2);
